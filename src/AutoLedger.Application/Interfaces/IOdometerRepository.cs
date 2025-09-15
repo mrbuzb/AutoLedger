6c048403fab4b9bb94592cd6aa1decccb6d1bf76
mrbuzb <mrbpubg14@gmail.com>
2025-09-15 08:06:56 +0500

    Completed

---
+++ src/AutoLedger.Application/Interfaces/IOdometerRepository.cs
@@ -0,0 +1,12 @@
+﻿using AutoLedger.Domain.Entities;
+
+namespace AutoLedger.Application.Interfaces;
+
+public interface IOdometerRepository
+{
+    Task<long> AddOdometerAsync(Odometer odometer);
+    Task DeleteOdometerAsync(long odometerId, long userId);
+    Task UpdateOdometerAsync(Odometer odometer);
+    Task<ICollection<Odometer>> GetOdometersByVehicleIdAsync(long vehicleId);
+    Task<Odometer> GetOdometerByIdAsync(long odometerId);
+}
---
+++ src/AutoLedger.Infrastructure/Persistence/Repositories/OdometerRepository.cs
@@ -0,0 +1,48 @@
+﻿using AutoLedger.Application.Interfaces;
+using AutoLedger.Core.Errors;
+using AutoLedger.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoLedger.Infrastructure.Persistence.Repositories;
+
+public class OdometerRepository(AppDbContext _context) : IOdometerRepository
+{
+    public async Task<long> AddOdometerAsync(Odometer odometer)
+    {
+        await _context.Odometers.AddAsync(odometer);
+        await _context.SaveChangesAsync();
+        return odometer.Id;
+    }
+
+    public async Task DeleteOdometerAsync(long odometerId, long userId)
+    {
+        var odometer = await _context.Odometers.Include(x=>x.Vehicle).FirstOrDefaultAsync(x=>x.Id == odometerId);
+        if(odometer == null || odometer.Vehicle.UserId != userId)
+        {
+            throw new NotAllowedException();
+        }
+        _context.Odometers.Remove(odometer);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<Odometer> GetOdometerByIdAsync(long odometerId)
+    {
+        var odometer = await _context.Odometers.FindAsync(odometerId);
+        if(odometer == null)
+        {
+            throw new EntityNotFoundException($"Odometer not found with id {odometerId}");
+        }
+        return odometer;
+    }
+
+    public async Task<ICollection<Odometer>> GetOdometersByVehicleIdAsync(long vehicleId)
+    {
+        return await _context.Odometers.Where(x=>x.VehicleId == vehicleId).ToListAsync();
+    }
+
+    public async Task UpdateOdometerAsync(Odometer odometer)
+    {
+        _context.Odometers.Update(odometer);
+        await _context.SaveChangesAsync();
+    }
+}
