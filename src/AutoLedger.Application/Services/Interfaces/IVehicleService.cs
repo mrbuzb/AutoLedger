25a60d1437e455d5070d70adf587b84657bdec0d
mrbuzb <mrbpubg14@gmail.com>
2025-09-17 07:45:16 +0500

    VexicleServiceCompleted

---
+++ src/AutoLedger.Application/Services/Interfaces/IVehicleService.cs
@@ -0,0 +1,12 @@
+﻿using AutoLedger.Application.Dtos;
+
+namespace AutoLedger.Application.Services.Interfaces;
+
+public interface IVehicleService
+{
+    Task<long> AddVehicleAsync(VehicleCreateDto vehicle,long userId);
+    Task DeleteVehicleAsync(long vehicleId, long userId);
+    Task UpdateVehicleAsync(VehicleUpdateDto vehicle,long userId);
+    Task<VehicleDto> GetVehicleByIdAsync(long vehicleId);
+    Task<ICollection<VehicleDto>> GetAllVehicleAsync(long userId);
+}
